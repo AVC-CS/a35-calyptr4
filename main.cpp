cff456b38879419dd017cebeb572a34af9a10cb0
calyptr4 <alessandrabaires@gmail.com>
2026-03-12 23:36:40 -0700

    a35

--- main.cpp
+++ main.cpp
@@ -6,18 +6,20 @@ using namespace std;
 
 int main()
 {
-    int rdnum1, rdnum2, rdnum3;
+    int num1, num2, num3;
     int total = 0;
+    int average;
 
     // TODO: seed the random number generator with time(0)
-
+srand(time(0));
     // TODO: generate 3 random numbers between 0 and 99
-
+num1 = rand()%100; num2 = rand()%100; num3 = rand()%100;
     // TODO: compute the total summation
-
+total = num1 + num2 + num3;
+average = total / 3;
     // TODO: print the 3 random numbers on the first line
-
+cout << num1 << " " << num2 << " " << num3 << endl;
     // TODO: print the total and average (integer division) on the second line
-
+cout << total << " " << average << endl;
     return 0;
 }
